c031e434be9475ce24760e517d2df347fe170bc0
Leo Xia <leo.xia@student.education.wa.edu.au>
2026-01-31 21:36:36 +0800

    Period Table Dictionary I have made a periodic table dictionary named elements.py, it includes all elements up to krypton (36) with proton number, electrons, valency, valence electrons and type of element.

---
+++ Elements.py
@@ -0,0 +1,290 @@
+elements = {
+    "H" : {
+        "name" : "hydrogen",
+        "protons" : 1,
+        "electrons" : 1,
+        "valence_electons" : 1,
+        "valency" : 1,
+        "type" : "non-metal"
+    },
+    "He" : {
+        "name" : "helium",
+        "protons" : 2,
+        "electrons" : 2,
+        "valence_electons" : 2,
+        "valency" : 0,
+        "type" : "non-metal"
+    },
+    "Li" : {
+        "name" : "Lithium",
+        "protons" : 3,
+        "electrons" : 3,
+        "valence_electons" : 1,
+        "valency" : 1,
+        "type" : "metal"
+    },
+    "Be" : {
+        "name" : "Berylium",
+        "protons" : 4,
+        "electrons" : 4,
+        "valence_electons" : 2,
+        "valency" : 2,
+        "type" : "metal"
+    },
+    "B" : {
+        "name" : "boron",
+        "protons" : 5,
+        "electrons" : 5,
+        "valence_electons" : 3,
+        "valency" : 3,
+        "type" : "Metalloid"
+    },
+    "C" : {
+        "name" : "carbon",
+        "protons" : 6,
+        "electrons" : 6,
+        "valence_electons" : 4,
+        "valency" : 4,
+        "type" : "non-metal"
+    },
+    "N" : {
+        "name" : "Nitrogen",
+        "protons" : 7,
+        "electrons" : 7,
+        "valence_electons" : 5,
+        "valency" : -3,
+        "type" : "non-metal"
+    },
+    "O" : {
+        "name" : "Oxygen",
+        "protons" : 8,
+        "electrons" : 8,
+        "valence_electons" : 6,
+        "valency" : -2,
+        "type" : "non-metal"
+    },
+    "F" : {
+        "name" : "fluorine",
+        "protons" : 9,
+        "electrons" : 9,
+        "valence_electons" : 7,
+        "valency" : -1,
+        "type" : "non-metal"
+    },
+    "Ne" : {
+        "name" : "Neon",
+        "protons" : 10,
+        "electrons" : 10,
+        "valence_electons" : 8,
+        "valency" : 0,
+        "type" : "non-metal"
+    },
+    "Na" : {
+        "name" : "sodiium",
+        "protons" : 11,
+        "electrons" : 11,
+        "valence_electons" : 1,
+        "valency" : 1,
+        "type" : "metal"
+    },
+    "Mg" : {
+        "name" : "magnesium",
+        "protons" : 12,
+        "electrons" : 12,
+        "valence_electons" : 2,
+        "valency" : 2,
+        "type" : "metal"
+    },
+    "Al" : {
+        "name" : "aluminium",
+        "protons" : 13,
+        "electrons" : 13,
+        "valence_electons" : 3,
+        "valency" : 3,
+        "type" : "metal"
+    },
+    "Si" : {
+        "name" : "hydrogen",
+        "protons" : 14,
+        "electrons" : 14,
+        "valence_electons" : 4,
+        "valency" : 4,
+        "type" : "metalloid"
+    },
+    "P" : {
+        "name" : "phosphorus",
+        "protons" : 15,
+        "electrons" : 15,
+        "valence_electons" : 5,
+        "valency" : -3,
+        "type" : "non-metal"
+    },
+    "S" : {
+        "name" : "sulfur",
+        "protons" : 16,
+        "electrons" : 16,
+        "valence_electons" : 6,
+        "valency" : -2,
+        "type" : "non-metal"
+    },
+    "Cl" : {
+        "name" : "chlorine",
+        "protons" : 17,
+        "electrons" : 17,
+        "valence_electons" : 7,
+        "valency" : -1,
+        "type" : "non-metal"
+    },
+    "Ar" : {
+        "name" : "argon",
+        "protons" : 18,
+        "electrons" : 18,
+        "valence_electons" : 8,
+        "valency" : 0,
+        "type" : "non-metal"
+    },
+    "K" : {
+        "name" : "potassium",
+        "protons" : 19,
+        "electrons" : 19,
+        "valence_electons" : 1,
+        "valency" : 1,
+        "type" : "metal"
+    },
+    "Ca" : {
+        "name" : "calcium",
+        "protons" : 20,
+        "electrons" : 20,
+        "valence_electons" : 2,
+        "valency" : 2,
+        "type" : "metal"
+    },
+    "Sc" : {
+        "name" : "scandium",
+        "protons" : 21,
+        "electrons" : 21,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Ti" : {
+        "name" : "titanium",
+        "protons" : 22,
+        "electrons" : 22,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "V" : {
+        "name" : "vanadium",
+        "protons" : 23,
+        "electrons" : 23,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Cr" : {
+        "name" : "chromium",
+        "protons" : 24,
+        "electrons" : 24,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Mn" : {
+        "name" : "manganese",
+        "protons" : 25,
+        "electrons" : 25,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Fe" : {
+        "name" : "iron",
+        "protons" : 26,
+        "electrons" : 26,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Co" : {
+        "name" : "cobalt",
+        "protons" : 27,
+        "electrons" : 27,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Ni" : {
+        "name" : "nickel",
+        "protons" :28,
+        "electrons" : 28,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Cu" : {
+        "name" : "copper",
+        "protons" : 29,
+        "electrons" : 29,
+        "valence_electons" : "variable",
+        "valency" : "variable",
+        "type" : "transition-metal"
+    },
+    "Zn" : {
+        "name" : "zinc",
+        "protons" :30,
+        "electrons" : 30,
+        "valence_electons" : 2,
+        "valency" : 2,
+        "type" : "transition-metal"
+    },
+    "Ga" : {
+        "name" : "gallium",
+        "protons" : 31,
+        "electrons" : 31,
+        "valence_electons" : 3,
+        "valency" : 3,
+        "type" : "metal"
+    },
+    "Ge" : {
+        "name" : "germanium",
+        "protons" : 32,
+        "electrons" : 32,
+        "valence_electons" : 4,
+        "valency" : 4,
+        "type" : "metalloid"
+    },
+    "As" : {
+        "name" : "arsenic",
+        "protons" : 33,
+        "electrons" : 33,
+        "valence_electons" : 5,
+        "valency" : -3,
+        "type" : "metalloid"
+    },
+    "Se" : {
+        "name" : "selenium",
+        "protons" : 34,
+        "electrons" : 34,
+        "valence_electons" : 6,
+        "valency" : -2,
+        "type" : "non-metal"
+    },
+    "Br" : {
+        "name" : "bromine",
+        "protons" : 35,
+        "electrons" : 35,
+        "valence_electons" : 7,
+        "valency" : -1,
+        "type" : "non-metal"
+    },
+    "Kr" : {
+        "name" : "krypton",
+        "protons" : 36,
+        "electrons" : 36,
+        "valence_electons" : 8,
+        "valency" : 0,
+        "type" : "non-metal"
+    },
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Chemistry-Calculator
+#Trying to make a calculator that can balance equations, identify solubility, colour and represent it visually
